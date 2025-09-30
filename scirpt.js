397370ed3e3b80a6aaa775d4c0430c0dd6cbb58d
prochris227 <promscitochris@gmail.com>
2025-09-30 09:53:30 +0100

    Adds index.html file

---
+++ scirpt.js
@@ -0,0 +1,11 @@
+let player1 = {
+    name: "Promise", 
+    Symbol: "X"
+};
+let player2 = {
+    name: "Jake", 
+    Symbol: "O"
+};
+document.getElementById("p1-name").textContent = player1.name;
+document.getElementById("p2-name").textContent = player2.name;
+
